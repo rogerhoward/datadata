54d98c3df8cbfc9a7a72b5537a067e9cc5f2ef5b
Roger Howard <rogerhoward@mac.com>
2017-09-20 16:15:57 -0700

    Working folder picker dialog

---
+++ renderer.js
@@ -0,0 +1,55 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+const { ipcRenderer, remote, shell } = require('electron');
+const { dialog } = remote;
+// const setApplicationMenu = require('./menu');
+
+// const form = document.querySelector('form');
+
+// const inputs = {
+//     source: form.querySelector('button[name="source"]'),
+//     // destination: form.querySelector('input[name="destination"]'),
+//     // name: form.querySelector('input[name="name"]'),
+//     // fps: form.querySelector('input[name="fps"]'),
+// };
+
+
+const buttons = {
+    source: document.getElementById('chooseSource'),
+};
+
+// ipcRenderer.on('did-finish-load', () => {
+//     setApplicationMenu();
+// });
+
+// ipcRenderer.on('processing-did-succeed', (event, html) => {
+//     shell.openExternal(`file://${html}`);
+// });
+
+// ipcRenderer.on('processing-did-fail', (event, error) => {
+//     console.error(error);
+//     alert('Failed :\'(');
+// });
+
+buttons.source.addEventListener('click', () => {
+    const directory = dialog.showOpenDialog({
+        properties: ['openDirectory'],
+    });
+    if (directory) {
+        // inputs.source.value = directory;
+        alert(directory);
+    }
+});
+
+
+// form.addEventListener('submit', (event) => {
+//     event.preventDefault();
+//     ipcRenderer.send('did-submit-form', {
+//         source: inputs.source.value,
+//         destination: inputs.destination.value,
+//         name: inputs.name.value,
+//         fps: inputs.fps.value,
+//     });
+// });
